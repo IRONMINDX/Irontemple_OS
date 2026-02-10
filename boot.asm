cc8c9c34445e61eae0fe81aec0e23179062322ef
IRONMINDX <subash94th@gmail.com>
2026-02-10 15:05:02 +0000

    changed bootlader from printing separate charectors to Strings

--- boot.asm
+++ boot.asm
@@ -1,13 +1,18 @@
-org 0x7C00          
+org 0x7C00         
 
-mov ah, 0x0E        
-mov al, 'X'
-int 0x10
+mov si, msg        
+print_loop:
+    lodsb         
+    cmp al, 0     
+    je done
+    mov ah, 0x0E    
+    int 0x10       
+    jmp print_loop
 
-mov al, 'i'
-int 0x10
+done:
+    jmp $           
+msg db 'Hello World',0  
 
-jmp $             
-
-times 510-($-$$) db 0
-dw 0xAA55           
+times 510-($-$$) db 0   
+dw 0xAA55              
+      
